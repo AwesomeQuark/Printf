017db1d4311a4f86d55442ff8ac8061cd8f1bc8a
Corentin NOEL <conoel@e3r7p22.42.fr>
2019-01-19 17:10:45 +0100

    oui

--- srcs/ft_itoa_float.c
+++ srcs/ft_itoa_float.c
@@ -6,13 +6,13 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 12:34:16 by conoel            #+#    #+#             */
-/*   Updated: 2019/01/15 10:14:30 by conoel           ###   ########.fr       */
+/*   Updated: 2019/01/17 02:26:56 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int		ft_size(long long nb)
+static int			ft_size(long long nb)
 {
 	int size;
 
@@ -34,20 +34,20 @@ static long long	ft_pow(long double num, int pow)
 	return ((long long)num);
 }
 
-void				printer(t_flag *all, long long nb, int size, int point_i)
+static void			printer(t_flag *all, long long nb, int size, int p_i)
 {
 	char	tmp[size + 1];
 	int		minsize;
 
 	ft_bzero(tmp, size + 1);
 	minsize = all->minsize - ft_size(nb);
-	while (all->minus == 1 && minsize-- > 0)
+	while (all->minus == 1 && minsize-- > 1)
 		tmp[--size] = all->zero ? '0' : ' ';
 	while (nb > 0)
 	{
 		tmp[--size] = nb % 10 + 48;
 		nb /= 10;
-		size == point_i ? tmp[--size] = '.' : 0;
+		size == p_i ? tmp[--size] = '.' : 0;
 	}
 	while (all->minus == 0 && minsize-- > 0)
 		tmp[--size] = all->zero ? '0' : ' ';
@@ -64,7 +64,7 @@ void				ft_ftoa(long double num, t_flag *all)
 	nb = ft_pow(num, all->precision);
 	size = ft_size(nb);
 	point_i = ft_size(num);
-	point_i += all->minsize > size && all->minus == 0 ? all->minsize - size : 0;
-	size = size > all->minsize ? size : all->minsize;
+	point_i += all->minsize > size && all->minus == 0 ? all->minsize - size : 1;
+	size = size > all->minsize ? size + 1 : all->minsize;
 	printer(all, nb, size, point_i);
 }
